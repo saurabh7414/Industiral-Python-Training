95d935888c2ce866d287ef81b4537972c368567e
saurabh7414 <saurabh886020@gmail.com>
2026-04-09 09:42:05 +0530

    Time: 240 ms (19.89%) | Memory: 25.9 MB (43.16%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        imbalance = 0
+        max_imbalance = 0
+        for char in s:
+            if char == '[':
+                imbalance -= 1
+            else: 
+                imbalance += 1
+            max_imbalance = max(max_imbalance, imbalance)
+        return (max_imbalance + 1) // 2
